67b76529be5f7910ae4217e817e25a9df33a5909
kingsonn <hansonnnnnnn@gmail.com>
2024-12-15 21:13:51 +0530

    yo

--- components/Providers.jsx
+++ components/Providers.jsx
@@ -1,5 +1,48 @@
 'use client';
 
+import { createContext, useContext, useEffect, useState } from 'react';
+import { createClient } from '../utils/supabase/client';
+
+const AuthContext = createContext();
+
+export function useAuth() {
+  return useContext(AuthContext);
+}
+
 export function Providers({ children }) {
-  return children;
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const supabase = createClient();
+
+  useEffect(() => {
+    // Check active sessions and sets the user
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setUser(session?.user ?? null);
+      setLoading(false);
+    });
+
+    // Listen for changes on auth state (logged in, signed out, etc.)
+    const { data: { subscription } } = supabase.auth.onAuthStateChange(async (event, session) => {
+      setUser(session?.user ?? null);
+      setLoading(false);
+    });
+
+    return () => subscription.unsubscribe();
+  }, []);
+
+  const value = {
+    user,
+    loading,
+    supabase
+  };
+
+  return (
+    <Provider >
+      <PersistGate loading={null} persistor={persistor}>
+        <AuthContext.Provider value={value}>
+          {!loading && children}
+        </AuthContext.Provider>
+      </PersistGate>
+    </Provider>
+  );
 }
